3338fee57177942b04c48b31ecf50edf22031fef
Adam Husrew <Adam.Husrew@cydeo.com>
2022-09-29 11:04:42 +0300

    Lab 8 Started

---
+++ src/lab08_SplitterApp/lab8Class/Main.java
@@ -0,0 +1,31 @@
+package lab08_SplitterApp.lab8Class;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+
+        System.out.println("Welcome to the budget splitter");
+
+        //Ask how many ppl in the budget planner?
+
+        System.out.println("How many people will split the budget");
+        int userCount= scanner.nextInt();
+
+
+        for (int i = 0;i< userCount; i++){
+            String userName1="ozzy";
+            String userEmail1="ozzy@gmail.com";
+
+            String userName2="mike";
+            String userEmail2="mike@gmail.com";
+
+
+
+
+        }
+
+    }
+}
